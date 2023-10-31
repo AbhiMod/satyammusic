8c21f4ba4c1f8854419a866a7e6d8a40529eba27
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-10-31 19:39:34 +0530

    Update am.py

--- am.py
+++ am.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("cd && rm -rf satyammusic && git clone https://github.com/AbhiMod/satyammusic && cd satyammusic && python3 -m AMMusicV2")
+system ("cd && rm -rf satyammusic && git clone https://github.com/AbhiMod/satyammusic && cd satyammusic && python3 -m DAXXMUSIC")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
