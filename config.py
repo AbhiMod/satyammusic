c86df456280e57583b7093e7334e334fcdce9aea
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-10-31 20:04:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/DAXXTEAM/DAXXMUSIC",
+    "https://github.com/AbhiMod/satyammusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
