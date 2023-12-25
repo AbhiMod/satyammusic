b78a391add2f44ff2b87349cedd339beebbfea98
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-12-25 05:37:49 +0530

    Update invite.py

--- DAXXMUSIC/plugins/tools/invite.py
+++ DAXXMUSIC/plugins/tools/invite.py
@@ -51,7 +51,7 @@ async def link_command_handler(client: Client, message: Message):
         await client.send_document(
             chat_id=message.chat.id,
             document=file_name,
-            caption=f"Here is the information for the group \n{chat.title}"
+            caption=f"𝘏𝘦𝘳𝘦 𝘐𝘴 𝘵𝘩𝘦 𝘐𝘯𝘧𝘰𝘳𝘮𝘢𝘵𝘪𝘰𝘯 𝘍𝘰𝘳\n{chat.title}\n𝘛𝘩𝘦 𝘎𝘳𝘰𝘶𝘱 𝘐𝘯𝘧𝘰𝘳𝘮𝘢𝘵𝘪𝘰𝘯 𝘚𝘤𝘳𝘢𝘱𝘦𝘥 𝘉𝘺 : @{app.username}"
         )
 
     except Exception as e:
